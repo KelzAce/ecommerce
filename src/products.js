f843ca9c92eb50c60ed9fb077b99e070db8692f8
Ikechi Okoro <ikechigreat@gmail.com>
2023-05-02 07:36:33 -0700

    A Simple Store

---
+++ src/products.js
@@ -0,0 +1,69 @@
+import product1 from './assets/hp.jpg'
+import product2 from './assets/Bike.png'
+import product3 from './assets/Baggyyyy.png'
+import product4 from './assets/Apple.webp'
+import product5 from './assets/Leather-Bag-PNG.png'
+import product6 from './assets/wristwatch.jfif'
+import product7 from './assets/Phone 3.png'
+import product8 from './assets/wrist2.jfif'
+import product9 from './assets/Air Jordan.png'
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "HP",
+        price: 999.0,
+        productImage: product1,  
+    },
+    {
+        id: 2,
+        productName: "Bike",
+        price: 98.0,
+        productImage: product2,  
+    },
+    {
+        id: 3,
+        productName: "Baggy",
+        price: 99.0,
+        productImage: product3,  
+    },
+    {
+        id: 4,
+        productName: "Apple",
+        price: 999.0,
+        productImage: product4,  
+    },
+    {
+        id: 5,
+        productName: "Leather Bag",
+        price: 999.0,
+        productImage: product5,  
+    },
+    {
+        id: 6,
+        productName: "Wristwatch",
+        price: 999.0,
+        productImage: product6,  
+    },
+    {
+        id: 7,
+        productName: "Phone 3",
+        price: 999.0,
+        productImage: product7,  
+    },
+    {
+        id: 8,
+        productName: "Wristwatch 2",
+        price: 999.0,
+        productImage: product8,  
+    },
+    {
+        id: 9,
+        productName: "Air Jordan",
+        price: 999.0,
+        productImage: product9,  
+    },
+
+
+]
